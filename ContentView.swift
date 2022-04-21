2a672a3b97a09189f1bbb1fe2105440088acf67e
Nur Mutmainnah Rahim <18319006@std.stei.itb.ac.id>
2022-04-21 14:52:23 +0700

    change background

--- ContentView.swift
+++ ContentView.swift
@@ -3,12 +3,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            Spacer()
             Text("INSECURITY")
                 .font(.largeTitle)
             Text("APPS")
                 .font(.largeTitle)
             Image("memoji")
             Text("How are you")
+            Spacer()
         }
+        .background(Color(red: 0.9, green: 251, blue: 152))
     }
 }
