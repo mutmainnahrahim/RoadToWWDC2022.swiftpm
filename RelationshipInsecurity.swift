90c67238aa9167da5ec4d6b0f1aad9be9f89faef
Nur Mutmainnah Rahim <18319006@std.stei.itb.ac.id>
2022-04-23 22:23:29 +0700

    Relationship page

--- RelationshipInsecurity.swift
+++ RelationshipInsecurity.swift
@@ -11,9 +11,9 @@ struct RelationshipInsecurity: View {
 
     var body: some View {
         VStack {
+            Image("Relationship")
             ScrollView {
-                Image("memoji")
-                Text("Saya hasdhkasdjkadadjh hjsahdakdjladndnadjkajdadla kdjahasjahaladaad. dshakjd ahdwjeka klaskaldad hjdhaueajka jskakslak")
+                Text("Kepolisian Resor Siak melakukan pengungkapan kasus penemuan mayat seorang gadis 16 tahun, di Kampung Benteng Hilir, Kecamatan Mempura, Kabupaten Siak yang ternyata merupakan tindakan pemerkosaan dan pembunuhan anak di bawah umur.“Kita berhasil mengamankan seorang tersangka pelaku tunggal kasus pembunuhan dan pemerkosaan, SAS (16). Pelaku sudah mengakui perbuatannya,” kata Kepala Polres Siak, AKBP Gunar Rahardianto, Senin (7/2).Dia mengatakan penangkapan pelaku dilakukan sesudah melalui rangkaian penyidikan dan dibantu Kepolisian Daerah Riau. Pelaku ditangkap di tepi jalan yang masih berada di daerah Benteng Hilir.Yang bersangkutan juga tidak ada upaya untuk melarikan diri sejak kabar kehilangan korban pada Rabu (2/2).“Dia ditangkap dalam kondisi kebingungan di tepi jalan,” ungkap Kapolres didampingi Kepala Satuan Kriminal dan Reserse Kriminal Polres Siak, Iptu Rachmat Wibowo.Dikatakan Kapolres, dugaan motif pembunuhan ini hanya ingin memperkosa korban. Dan karena takut ketahuan pelaku memutuskan untuk juga membunuh dan menguburkan korban.Awalnya, korban melalui aplikasi pesan meminjam uang kepada pelaku Rp500 ribu.Korban katanya punya utang sama temannya di Pekanbaru lalu mencari pinjaman lain hingga sampai ke pelaku.Pelaku dan korban pernah pacaran juga namun putus hubungan November 2021.Kasus ini mulai mengemuka sejak Rabu (2/2), keluarga korban melaporkan kehilangan anak.Keluarga korban yang merupakan warga Kampung Paluh, Kecamatan Mempura, selain melapor polisi juga melakukan pencarian.Akhirnya, pada Minggu (6/2) ditemukan sesosok jenazah yang dikebumikan sedalam 40 centimeter di kebun sawit, Kampung Benteng Hilir.Awalnya ditemukan oleh ayah tiri tersangka, Hendrik lalu melaporkan ke kepolisian hingga 12 jam kemudian pelaku akhirnya juga ditangkap")
                     .font(.largeTitle)
 
             }
