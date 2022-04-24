14377f378e25bd362bb1574f93b4da94f7a11609
Nur Mutmainnah Rahim <18319006@std.stei.itb.ac.id>
2022-04-24 15:46:21 +0700

    fineshed 1/2

--- ContentView.swift
+++ ContentView.swift
@@ -1,17 +1,54 @@
 import SwiftUI
 
-
 struct ContentView: View {
-    
+ 
     var body: some View {
         VStack {
-            Text("INSECURITY APPS")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-            Image("memoji2")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-            Text("Hi, do you feel sad again?")
+            TabView {
+                HomeView()
+                    .tabItem {
+                        Image(systemName: "house")
+                        Text("Home")
+                        
+                    }
+
+                BodyInsecurity()
+                    .tabItem {
+                        Label("Body", systemImage: "eyebrow")
+                    }
+                
+                FinancialInsecurity()
+                    .tabItem {
+                        Label("Financial", systemImage: "dollarsign.circle")
+                    }
+                
+                HealthInsecurity()
+                    .tabItem {
+                        Label("Health", systemImage: "figure.roll")
+                    }
+                
+                JobInsecurity()
+                    .tabItem {
+                        Label("Job", systemImage: "bag")
+                    }
+                
+                RelationshipInsecurity()
+                    .tabItem {
+                        Label("Relationship", systemImage: "heart.circle")
+                    }
+                
+                SocialInsecurity()
+                    .tabItem {
+                        Label("Social", systemImage: "person.3")
+                    }
+            }
         }
     }
 }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ HomeView.swift
@@ -0,0 +1,46 @@
+//
+//  File.swift
+//  RoadToWWDC2022
+//
+//  Created by Nur Mutmainnah Rahim on 24/04/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+ 
+    var body: some View {
+        VStack {
+            Text("INSECURITY APPS")
+                .font(.system(size: 80, weight: .light, design: .serif))
+                .fontWeight(.bold)
+            
+            Image("memoji")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 700, height: 700, alignment: .top)
+        
+            Text("Hi, do you feel sad again?")
+                .font(.system(size: 40, weight: .light, design: .serif))
+                .padding()
+            
+            Button(action: {
+            }) {
+                Text("Click button below if you are insecure")
+                    .font(.system(size: 25, weight: .bold, design: .default))
+                    .padding()
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .padding(10)
+                    .border(Color.green, width: 5)
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
+
